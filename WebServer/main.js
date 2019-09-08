ce784cc896959ea3f8a078311c47731fd288b9c7
Andrew Pasquale <andrew@elytra.net>
2019-09-08 18:32:07 -0400

    Use port 8000 for development and callback function after starting the server.

--- WebServer/main.js
+++ WebServer/main.js
@@ -6,10 +6,10 @@ app.get('/oauth/redirect', (req, res) => {
     
     let query = req.query;
     console.log(query);
-
     res.sendFile(__dirname + '/public/connected.html');
 })
 
 app.use(express.static(__dirname + '/public'))
-app.listen(80)
-console.log("Webserver Running")
+app.listen(8000, () => {
+    console.log("Webserver Running")
+})
