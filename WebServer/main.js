e05a187b589123a4bbc1b061f624d438f7bf47e1
Andrew Pasquale <andrew@elytra.net>
2019-09-08 22:29:13 -0400

    Token is return as a hash parameter, only visible in the browser.

--- WebServer/main.js
+++ WebServer/main.js
@@ -3,9 +3,6 @@ const express = require('express')
 const app = express()
 
 app.get('/oauth/redirect', (req, res) => {
-    
-    let query = req.query;
-    console.log(query);
     res.sendFile(__dirname + '/public/connected.html');
 })
 
