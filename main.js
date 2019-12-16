fc0e79932d9610e201efbced51ecaa3ae9974600
Jacob Bashista <jacob@orange.haus>
2019-12-16 18:07:35 -0500

    Updated with Twitch Login

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.get('/oauth/redirect', (req, res) => {
-    res.sendFile(__dirname + '/public/connected.html');
+    res.sendFile(path.join(__dirname + '/public/connected.html'));
 })
 
 app.use(express.static(__dirname + '/public'))
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log("Webserver Running")
 })
